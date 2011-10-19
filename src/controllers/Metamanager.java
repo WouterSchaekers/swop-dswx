80c76b48447a3a6093eb22fd0f820c4b1882888f
vegetas.fate@gmail.com <vegetas.fate@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-10-19 14:40:30 +0000

    Implemented metamanager using the CONSTRUCTOR for initialization; added documentation.

--- src/controllers/Metamanager.java
+++ src/controllers/Metamanager.java
@@ -4,28 +4,36 @@ import java.util.HashMap;
 import java.util.Map;
 import users.UserManager;
 
+/**
+ * This class manages all the managers used in this software package.
+ * @author Stefaan, Dieter
+ *
+ */
 public class Metamanager
 {
-	private Map<String,Controller> controllers;
-	private Map<String,Manager> manager;
-	public Metamanager()
-	{
-		
-	}
-	public void intializeSystem()
-	{
-		controllers =new HashMap<String, Controller>();
+	private Map<String, Controller> controllers;
+	private Map<String, Manager> manager;
+
+	public Metamanager() {
+		controllers = new HashMap<String, Controller>();
 		manager = new HashMap<String, Manager>();
 		controllers.put("LoginController", new LoginController(this));
 		manager.put("UserManager", new UserManager());
 	}
-	public Controller getController(String name)
-	{
-		Controller s =controllers.get(name);
+
+	/**
+	 * @param name
+	 * the name of the requested controller 
+	 * @return
+	 * returns the requested controller 
+	 */
+	public Controller getController(String name) {
+		Controller s = controllers.get(name);
 		return s;
 	}
-	public <T extends Manager> T getManager(String manager)
-	{
+
+	@SuppressWarnings("unchecked")
+	public <T extends Manager> T getManager(String manager) {
 		return (T) this.manager.get(manager);
 	}
 }
