5f4852a64e7e8dd2e919cc9d76078d52a0afac0b
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-10-28 12:34:16 +0000

    Schedulers merged.

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -2,18 +2,26 @@ package scheduler;
 
 import help.DateCalculator;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
+import machine.Machine;
+import medicaltest.MedicalTest;
 import patient.PatientFile;
 import users.Doctor;
 
-public class AppointmentScheduler
+public class Scheduler
 {
-	public AppointmentScheduler(){
+	public Scheduler(){
 		appointments = new HashMap<Doctor, ArrayList<Appointment>>();
+		medicalTests = new HashMap<PatientFile, ArrayList<MedicalTest>>();
+		treatments = new HashMap<PatientFile, ArrayList<Machine>>();
 	}
 	
 	private HashMap<Doctor, ArrayList<Appointment>> appointments;
+	private HashMap<PatientFile, ArrayList<MedicalTest>> medicalTests;
+	private HashMap<PatientFile, ArrayList<Machine>> treatments;
 	private static int appointmentDuration = 30;
 	
 	public Appointment addAppointment(PatientFile patient, Doctor doctor){
@@ -44,5 +52,15 @@ public class AppointmentScheduler
 		Date latestDate = getLatestAppointmentMoment(doctor);
 		Date newDate = DateCalculator.addTimeToDate(latestDate, appointmentDuration*60);
 		return newDate;
+	}
+
+	public void addMedicalTest(PatientFile patient, MedicalTest medicalTest) {
+		Collection<PatientFile> patientFileCollection = medicalTests.keySet();
+		Iterator<PatientFile> patientFileIterator = patientFileCollection.iterator();
+		while (patientFileIterator.hasNext()) {
+			PatientFile currentPatient = (PatientFile) patientFileIterator.next();
+
+		}
+		// To be implemented
 	}
 }
