a736ab55ac78115ee49655664ad541e94ccb98d1
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-10-28 08:44:29 +0000

    Updated addMedicalTest

--- src/medicaltest/BloodAnalysis.java
+++ src/medicaltest/BloodAnalysis.java
@@ -3,5 +3,8 @@ package medicaltest;
 
 public class BloodAnalysis extends MedicalTest
 {
-
+	public static final String TESTNAME = "BloodAnalysis";
+	public BloodAnalysis(){
+		super(TESTNAME);
+	}
 }
--- src/medicaltest/XRayScan.java
+++ src/medicaltest/XRayScan.java
@@ -2,5 +2,8 @@ package medicaltest;
 
 public class XRayScan extends MedicalTest
 {
-
+	public static final String TESTNAME = "XRayScan";
+	public XRayScan(){
+		super(TESTNAME);
+	}
 }
