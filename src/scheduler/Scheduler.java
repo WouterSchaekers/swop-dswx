8dbcce431aa0929694c727270f4577b059630de8
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-10-29 15:57:22 +0000

    Important scheduler update.

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -22,9 +22,11 @@ public class Scheduler
 	private HashMap<Date, ArrayList<MedicalTest>> medicalTests;
 	private HashMap<Date, ArrayList<Machine>> treatments;
 
-	public Appointment addAppointment(PatientFile patient, Doctor doctor, int appointmentDuration) {
+	public Appointment addAppointment(PatientFile patient, Doctor doctor,
+			int appointmentDuration) {
 		Date nextDate = getNextAppointmentMoment(doctor);
-		Appointment appointment = new Appointment(patient, doctor, nextDate, appointmentDuration);
+		Appointment appointment = new Appointment(patient, doctor, nextDate,
+				appointmentDuration);
 		appointments.get(doctor).add(appointment);
 		return appointment;
 	}
@@ -44,38 +46,51 @@ public class Scheduler
 		return doctorAppointments;
 	}
 
-	private Appointment getLatestAppointmentMoment(Doctor doctor) {
+	private Appointment getLatestAppointment(Doctor doctor) {
 		ArrayList<Appointment> doctorAppointments = getAppointments(doctor);
-		if(doctorAppointments.size() == 0){
-			return null;
-		}
-		Appointment latestAppointment = doctorAppointments.get(0);
-		Date latestDate = latestAppointment.getDate();
+		Appointment latestAppointment = null;
+		Date latestDate = null;
 		for (Appointment appointment : doctorAppointments) {
 			Date currentDate = appointment.getDate();
-			if (currentDate.after(latestDate)) {
+			if(latestDate == null || currentDate.after(latestDate)) {
 				latestDate = currentDate;
+				latestAppointment = appointment;
 			}
 		}
 		return latestAppointment;
 	}
 
-	public Date getNextAppointmentMoment(Doctor doctor) {
-		Appointment latestAppointment = getLatestAppointmentMoment(doctor);
+	private Date getNextAppointmentMoment(Doctor doctor) {
+		Appointment latestAppointment = getLatestAppointment(doctor);
 		Date latestDate = latestAppointment.getDate();
 		int timeToAdd = latestAppointment.getAppointmentDuration();
 		Date newDate = DateCalculator.addTimeToDate(latestDate, timeToAdd);
 		return newDate;
 	}
 
-//	 public void addMedicalTest(PatientFile patient, MedicalTest medicalTest){
-//		 Collection<Date> patientFileCollection = medicalTests.keySet();
-//		 Iterator<PatientFile> patientFileIterator =
-//		 patientFileCollection.iterator();
-//		 while (patientFileIterator.hasNext()) {
-//		 PatientFile currentPatient = (PatientFile) patientFileIterator.next();
-//		
-//		 }
-//		 To be implemented
-//	 }
+	public void addMedicalTest(MedicalTest medicalTest) {
+	}
+	
+	private MedicalTest getLatestMedicalTest(MedicalTest medicalTest){
+		Collection<Date> dates = this.medicalTests.keySet();
+		MedicalTest latestMedicalTest = null;
+		Date latestDate = null;
+		for (Date date : dates) {
+			ArrayList<MedicalTest> curMedicalTests = medicalTests.get(date);
+			for (MedicalTest curMedicalTest : curMedicalTests){
+				if(curMedicalTest.getTestName().equals(medicalTest.getTestName())){
+					Date curDate = curMedicalTest.getDate();
+					if(latestDate == null || curDate.after(latestDate)){
+						latestDate = curDate;
+						latestMedicalTest = curMedicalTest;
+					}
+				}
+			}
+		}
+		return latestMedicalTest;
+	}
+	
+	private Date getNextMedicalTestMoment(MedicalTest medicalTest){
+		return null;
+	}
 }
