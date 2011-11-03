f0f25a0c385041b953f7dbd5bde0b466005153b1
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-11-03 03:53:28 +0000

    More tests! Yeah!

--- src/controllers/MedicalTestControllerTest.java
+++ src/controllers/MedicalTestControllerTest.java
@@ -15,42 +15,59 @@ public class MedicalTestControllerTest
 {
 	UserManager usm;
 	MachinePool mp;
-	Scheduler scheduler;
-	TaskManager tsm;
-	PatientFileManager pfm;
-	DataPasser dp;
-	LoginController lc;
 	User u;
-	ConsultPatientFileController cpfc;
 	DTOUser dtouser;
-	MedicalTestController mtc;
+	PatientFileManager pfm;
 	
 	@Before
 	public void setUp(){
 		usm = new UserManager();
 		mp = new MachinePool();
-		scheduler = new Scheduler(usm, mp);
-		tsm = new TaskManager(scheduler);
-		pfm = new PatientFileManager();
-		dp = new DataPasser(usm, pfm, scheduler);
-		lc = new LoginController(dp);
 		u = new Doctor("Dude");
-		cpfc = new ConsultPatientFileController(pfm, u);
 		dtouser = new DTOUser(u);
-		lc.logIn(dtouser);
-		mtc = new MedicalTestController(lc, cpfc, dp);
+		pfm = new PatientFileManager();
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void creationFail() throws IllegalArgumentException{
+		Scheduler scheduler = new Scheduler(usm, mp);
+		DataPasser dp = new DataPasser(usm, pfm, scheduler);
+		LoginController lc = new LoginController(dp);
+		lc.logIn(dtouser);
 		new MedicalTestController(lc, null, dp);
 	}
 	
-	@Test
-	public void orderXRay(){
-		String bodypart = "Bionic hand";
-		int amountOfImages = 77;
-		int zoomLevel = 100;
-		
+	@Test(expected = IllegalArgumentException.class)
+	public void creationFail2() throws IllegalArgumentException{
+		Scheduler scheduler = new Scheduler(usm, mp);
+		DataPasser dp = new DataPasser(usm, pfm, scheduler);
+		LoginController lc = new LoginController(dp);
+		lc.logIn(dtouser);
+		PatientFileOpenController pfoc = new PatientFileOpenController(dp, lc);
+		ConsultPatientFileController cpfc = new ConsultPatientFileController(pfm, u, lc, pfoc);
+		new MedicalTestController(null, cpfc, dp);
 	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void creationFail3() throws IllegalArgumentException{
+		Scheduler scheduler = new Scheduler(usm, mp);
+		DataPasser dp = new DataPasser(usm, pfm, scheduler);
+		LoginController lc = new LoginController(dp);
+		PatientFileOpenController pfoc = new PatientFileOpenController(dp, lc);
+		ConsultPatientFileController cpfc = new ConsultPatientFileController(pfm, u, lc, pfoc);
+		new MedicalTestController(lc, cpfc, dp);
+	}
+	
+	@Test
+	public void creationSuccess(){
+		Scheduler scheduler = new Scheduler(usm, mp);
+		DataPasser dp = new DataPasser(usm, pfm, scheduler);
+		LoginController lc = new LoginController(dp);
+		lc.logIn(dtouser);
+		TaskManager tsm = new TaskManager(scheduler);
+		PatientFileOpenController pfoc = new PatientFileOpenController(dp, lc);
+		ConsultPatientFileController cpfc = new ConsultPatientFileController(pfm, u, lc, pfoc);
+		MedicalTestController mtc = new MedicalTestController(lc, cpfc, dp);
+	}
+	
 }
