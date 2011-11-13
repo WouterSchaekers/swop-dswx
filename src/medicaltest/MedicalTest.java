c5fb94be45830f36d21b2e046b0ae6fabdab5093
dietergeboers <dietergeboers@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-11-13 20:52:56 +0000

    small med test changes

--- src/medicaltest/MedicalTest.java
+++ src/medicaltest/MedicalTest.java
@@ -1,27 +1,33 @@
 package medicaltest;
 
+import be.kuleuven.cs.som.annotate.Basic;
+
 /**
  * This class represents a medical test.
  */
 public abstract class MedicalTest
 {	
 	// all childclasses will have their names be final and static and will use this var to store that information in.
-	private String testName = "";
-	
+
+	private final String TESTNAME;
+	private final int DURATION;
 	/**
 	 * Default constructor.
 	 * @param name
 	 * The name of this medical test.
 	 */
 	public MedicalTest(String name, int duration) {
-		this.testName = name;		
+		this.TESTNAME = name;	
+		this.DURATION=duration;
 	}
  
-	/**
-	 * @return The name of this medical test.
-	 */
+	@Basic
 	public String getTestName(){
-		return this.testName;
+		return this.TESTNAME;
+	}
+
+	public int getDURATION() {
+		return DURATION;
 	}
 	
 }
