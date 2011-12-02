bb479c3899a232d67b57242aa5d37c46c309459e
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-02 16:04:32 +0000

    Update of DepotManager.

--- src/depot/DepotManager.java
+++ src/depot/DepotManager.java
@@ -1,5 +1,8 @@
 package depot;
 
+import java.util.Collection;
+import exceptions.DepotOverCapacityException;
+
 public class DepotManager
 {
 	private Depot depot;
@@ -7,5 +10,15 @@ public class DepotManager
 		this.depot = depot;
 	}
 	
+	public void addPlaster(int units) throws DepotOverCapacityException{
+		depot.addPlaster(units);
+	}
 	
+	public void addMedication(Collection<Medication> medication) throws DepotOverCapacityException{
+		depot.addMedication(medication);
+	}
+	
+	public void addMeals(Collection<Meal> meals) throws DepotOverCapacityException{
+		depot.addMeals(meals);
+	}
 }
