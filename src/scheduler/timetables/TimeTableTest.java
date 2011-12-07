a0ab579c4d9f9db0939d61922243c622904bf4f9
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-07 16:40:38 +0000

    TimeTableTest is complete + intersectMethod works with equal start points.

--- src/scheduler/timetables/TimeTableTest.java
+++ src/scheduler/timetables/TimeTableTest.java
@@ -24,6 +24,13 @@ public class TimeTableTest
 		assertTrue(table1.equals(table2));
 	}
 	
+	@Test(expected = IllegalArgumentException.class)
+	public void argumentTestFail() {
+		@SuppressWarnings("unused")
+		TimeSlot t1 = new TimeSlot(new TimePoint(new Date(-2), TimeType.start), new TimePoint(new Date(-5), TimeType.end));
+		throw new IllegalStateException("Should not be able to get past all the previous code!!! ");
+	}
+	
 	@Test
 	public void unionTest() {
 		TimeSlot t1 = new TimeSlot(new TimePoint(new Date(0), TimeType.start),
@@ -68,26 +75,19 @@ public class TimeTableTest
 		TimeSlot t2 = new TimeSlot(new TimePoint(new Date(-2), TimeType.start), new TimePoint(new Date(15), TimeType.end));
 		TimeSlot t3 = new TimeSlot(new TimePoint(new Date(2), TimeType.start),new TimePoint(new Date(9), TimeType.end));
 		TimeSlot t4 = new TimeSlot(new TimePoint(new Date(13), TimeType.start), new TimePoint(new Date(21), TimeType.end));
-		TimeSlot t5= new TimeSlot(new TimePoint(new Date(130), TimeType.start), new TimePoint(new Date(2100), TimeType.end));
-		TimeSlot t6 = new TimeSlot(new TimePoint(new Date(-2), TimeType.start), new TimePoint(new Date(21), TimeType.end));
 		TimeTable table = new TimeTable(t1, t2);
 		TimeTable table2 = new TimeTable(t3, t4);
 		TimeTable res = table.getUnion(table2);
+		TimeSlot t5 = new TimeSlot(new TimePoint(new Date(130), TimeType.start), new TimePoint(new Date(2100), TimeType.end));
+		TimeSlot t6 = new TimeSlot(new TimePoint(new Date(-2), TimeType.start), new TimePoint(new Date(21), TimeType.end));
 		assertFalse(table.equals(new TimeTable(t5,t6)));
 		assertFalse(res.equals(new TimeTable(t5,t6)));
 		assertTrue(res.equals(new TimeTable(t1,t6)));
 
 	}
 	
-	@Test(expected = IllegalArgumentException.class)
-	public void union4TestFaal() {
-		@SuppressWarnings("unused")
-		TimeSlot t1 = new TimeSlot(new TimePoint(new Date(-2), TimeType.start), new TimePoint(new Date(-5), TimeType.end));
-		throw new IllegalStateException("Should not be able to get past all the previous code!!! ");
-	}
-	
 	@Test
-	public void union5Test() {
+	public void union4Test() {
 		TimeSlot t1 = new TimeSlot(new TimePoint(new Date(-5), TimeType.start), new TimePoint(new Date(-2), TimeType.end));
 		TimeSlot t2 = new TimeSlot(new TimePoint(new Date(2), TimeType.start), new TimePoint(new Date(15), TimeType.end));
 		TimeSlot t3 = new TimeSlot(new TimePoint(new Date(200), TimeType.start), new TimePoint(new Date(900), TimeType.end));
@@ -107,13 +107,71 @@ public class TimeTableTest
 		TimeSlot t2 = new TimeSlot(new TimePoint(new Date(10), TimeType.start), new TimePoint(new Date(15), TimeType.end));
 		TimeTable table = new TimeTable(t1, t2);
 		TimeTable table2 = new TimeTable(t1, t2);
-		TimeTable res = table.getUnion(table);
+		TimeTable res = table.getUnion(table2);
 		assertTrue(res.equals(table2));
 		assertTrue(res.equals(table));
 	}
 
 	@Test
-	public void intersect1Test() {
+	public void intersectTest() {
+		TimeSlot t1 = new TimeSlot(new TimePoint(new Date(0), TimeType.start),
+				new TimePoint(new Date(5), TimeType.end));
+		TimeSlot t2 = new TimeSlot(new TimePoint(new Date(8), TimeType.start),
+				new TimePoint(new Date(15), TimeType.end));
+		TimeSlot t3 = new TimeSlot(new TimePoint(new Date(2), TimeType.start),
+				new TimePoint(new Date(7), TimeType.end));
+		TimeSlot t4 = new TimeSlot(
+				new TimePoint(new Date(13), TimeType.start), new TimePoint(
+						new Date(21), TimeType.end));
+		TimeTable table = new TimeTable(t1, t2);
+		TimeTable table2 = new TimeTable(t3, t4);
+		TimeTable res = table.getIntersect(table2);
+		TimeSlot t6 = new TimeSlot(new TimePoint(new Date(2), TimeType.start), new TimePoint(new Date(5), TimeType.end));
+		TimeSlot t7 = new TimeSlot(new TimePoint(new Date(13), TimeType.start), new TimePoint(new Date(15), TimeType.end));
+		assertTrue(res.equals(new TimeTable(t6, t7)));
+	}
+
+	@Test
+	public void intersect2Test() {
+		TimeSlot t1 = new TimeSlot(new TimePoint(new Date(0), TimeType.start),
+				new TimePoint(new Date(5), TimeType.end));
+		TimeSlot t2 = new TimeSlot(new TimePoint(new Date(8), TimeType.start),
+				new TimePoint(new Date(15), TimeType.end));
+		TimeSlot t3 = new TimeSlot(new TimePoint(new Date(2), TimeType.start),
+				new TimePoint(new Date(9), TimeType.end));
+		TimeSlot t4 = new TimeSlot(
+				new TimePoint(new Date(13), TimeType.start), new TimePoint(
+						new Date(21), TimeType.end));
+		TimeTable table = new TimeTable(t1, t2);
+		TimeTable table2 = new TimeTable(t3, t4);
+		TimeTable res = table.getIntersect(table2);
+		TimeSlot t6 = new TimeSlot(new TimePoint(new Date(2), TimeType.start), new TimePoint(new Date(5), TimeType.end));
+		TimeSlot t7 = new TimeSlot(new TimePoint(new Date(8), TimeType.start), new TimePoint(new Date(9), TimeType.end));
+		TimeSlot t8 = new TimeSlot(new TimePoint(new Date(13), TimeType.start), new TimePoint(new Date(15), TimeType.end));
+		assertTrue(res.equals(new TimeTable(t6,t7,t8)));
+	}
+
+	@Test
+	public void intersect3Test() {
+		TimeSlot t1 = new TimeSlot(new TimePoint(new Date(-5), TimeType.start), new TimePoint(new Date(-2), TimeType.end));
+		TimeSlot t2 = new TimeSlot(new TimePoint(new Date(-2), TimeType.start), new TimePoint(new Date(15), TimeType.end));
+		TimeSlot t3 = new TimeSlot(new TimePoint(new Date(2), TimeType.start),new TimePoint(new Date(9), TimeType.end));
+		TimeSlot t4 = new TimeSlot(new TimePoint(new Date(13), TimeType.start), new TimePoint(new Date(21), TimeType.end));
+		TimeTable table = new TimeTable(t1, t2);
+		TimeTable table2 = new TimeTable(t3, t4);
+		TimeTable res = table.getIntersect(table2);
+		TimeSlot t5 = new TimeSlot(new TimePoint(new Date(130), TimeType.start), new TimePoint(new Date(2100), TimeType.end));
+		TimeSlot t6 = new TimeSlot(new TimePoint(new Date(-2), TimeType.start), new TimePoint(new Date(21), TimeType.end));
+		TimeSlot t7 = new TimeSlot(new TimePoint(new Date(2), TimeType.start), new TimePoint(new Date(9), TimeType.end));
+		TimeSlot t8 = new TimeSlot(new TimePoint(new Date(13), TimeType.start), new TimePoint(new Date(15), TimeType.end));
+		assertFalse(table.equals(new TimeTable(t5,t6)));
+		assertFalse(res.equals(new TimeTable(t5,t6)));
+		assertTrue(res.equals(new TimeTable(t7,t8)));
+
+	}
+	
+	@Test
+	public void intersect4Test() {
 		TimeSlot t1 = new TimeSlot(new TimePoint(new Date(-5), TimeType.start), new TimePoint(new Date(-2), TimeType.end));
 		TimeSlot t2 = new TimeSlot(new TimePoint(new Date(2), TimeType.start), new TimePoint(new Date(15), TimeType.end));
 		TimeSlot t3 = new TimeSlot(new TimePoint(new Date(200), TimeType.start), new TimePoint(new Date(900), TimeType.end));
@@ -121,8 +179,18 @@ public class TimeTableTest
 		TimeTable table = new TimeTable(t1, t2);
 		TimeTable table2 = new TimeTable(t3, t4);
 		TimeTable res = table.getIntersect(table2);
-		System.out.println(res.toString());
 		TimeSlot t5 = new TimeSlot(new TimePoint(new Date(13), TimeType.start), new TimePoint(new Date(15), TimeType.end));
 		assertTrue(res.equals(new TimeTable(t5)));
+	}
+	
+	@Test
+	public void intersectTestWithOnlyOneTable() {
+		TimeSlot t1 = new TimeSlot(new TimePoint(new Date(1), TimeType.start), new TimePoint(new Date(5), TimeType.end));
+		TimeSlot t2 = new TimeSlot(new TimePoint(new Date(10), TimeType.start), new TimePoint(new Date(15), TimeType.end));
+		TimeTable table = new TimeTable(t1, t2);
+		TimeTable table2 = new TimeTable(t1, t2);
+		TimeTable res = table.getIntersect(table2);
+		assertTrue(res.equals(table2));
+		assertTrue(res.equals(table));
 	}
 }
