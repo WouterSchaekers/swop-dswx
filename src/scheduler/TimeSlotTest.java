a7957c875a342dd1c82e8836262f27dcb407f553
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-10 16:21:31 +0000

    Overlaps works now for real!

--- src/scheduler/TimeSlotTest.java
+++ src/scheduler/TimeSlotTest.java
@@ -15,8 +15,17 @@ public class TimeSlotTest
 	}
 
 	@Test(expected = IllegalArgumentException.class)
-	public void stopBeforeStartTest() {
+	public void stopBeforeStartTestFail() {
 		TimeSlot s = new TimeSlot(new TimePoint(new Date(0), TimeType.stop),
 				new TimePoint(new Date(5), TimeType.start));
+	}
+	
+	@Test
+	public void overlapsTest(){
+		TimeSlot s0 = new TimeSlot(new TimePoint(new Date(0), TimeType.start),
+				new TimePoint(new Date(5), TimeType.stop));
+		TimeSlot s1 = new TimeSlot(new TimePoint(new Date(0), TimeType.start),
+				new TimePoint(new Date(5), TimeType.stop));
+		assertTrue(s0.overlaps(s1));
 	}
 }
