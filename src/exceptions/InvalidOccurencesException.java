1a65f3425a4f041e294cc2ee0b6ef2875417d5af
vegetas.fate@gmail.com <vegetas.fate@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-16 13:55:16 +0000

    Regarding the previous commit: updated the task-package with UnscheduledTask and ScheduledTask; updated TaskManager. This commit: added some exceptions.

---
+++ src/exceptions/InvalidOccurencesException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class InvalidOccurencesException extends Exception
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidOccurencesException(String arg0) {
+		super(arg0);
+	}
+
+}
