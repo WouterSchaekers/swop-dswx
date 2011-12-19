03f3042fc6ced7b17aded993098a3143303026c6
vegetas.fate@gmail.com <vegetas.fate@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-19 17:06:38 +0000

    Added documentation for TimePoints

--- src/scheduler/EndTimePoint.java
+++ src/scheduler/EndTimePoint.java
@@ -1,41 +1,59 @@
 package scheduler;
 
+/**
+ * This class represents a point in time that, specifically, is the end of a
+ * timeslot.
+ */
 public class EndTimePoint extends TimePoint
 {
-
+	/**
+	 * @param d
+	 * The date of this Endpoint.
+	 */
 	public EndTimePoint(HospitalDate d) {
 		super(d);
 	}
+
+	/**
+	 * @param l
+	 * The amount of milliseconds since the start of time.
+	 */
 	public EndTimePoint(long l) {
 		super(l);
 	}
-	public EndTimePoint(TimePoint t)
-	{
+
+	/**
+	 * @param t
+	 * Another timepoint that serves as the base for this endpoint.
+	 */
+	public EndTimePoint(TimePoint t) {
 		super(t);
 	}
+
 	@Override
-	public boolean isEnd()
-	{
+	public boolean isEnd() {
 		return true;
 	}
+
 	@Override
-	public boolean isStart()
-	{
+	public boolean isStart() {
 		return false;
 	}
+
 	@Override
 	public TimePoint clone() {
 		return new EndTimePoint(this);
 	}
+
 	@Override
 	public String toString() {
 		return "End: " + this.getDate();
 	}
+
 	@Override
-	public boolean equals(Object o)
-	{
-		if(!(o instanceof EndTimePoint))
+	public boolean equals(Object o) {
+		if (!(o instanceof EndTimePoint))
 			return false;
-		return this.getDate().equals(((EndTimePoint)o).getDate());
+		return this.getDate().equals(((EndTimePoint) o).getDate());
 	}
 }
--- src/scheduler/StartTimePoint.java
+++ src/scheduler/StartTimePoint.java
@@ -1,16 +1,35 @@
 package scheduler;
 
+/**
+ * This class represents a point in time that is, specifically, the start of a
+ * TimeSlot.
+ */
 public class StartTimePoint extends TimePoint
 {
-
+	/**
+	 * Constructor where a HospitalDate is given.
+	 * @param d
+	 * The date of this time point.
+	 */
 	public StartTimePoint(HospitalDate d) {
 		super(d);
 	}
 
+	/**
+	 * Constructor where the amount of millis since the system start have been
+	 * given to.
+	 * 
+	 * @param timeInMillis
+	 *            The amount of milliseconds since the sytem start.
+	 */
 	public StartTimePoint(long timeInMillis) {
 		super(timeInMillis);
 	}
 
+	/**
+	 * Use this constructor if you would like to have this StartTimePoint be a clone of another TimePoint.(same date)
+	 * @param t
+	 */
 	public StartTimePoint(TimePoint t) {
 		super(t);
 	}
