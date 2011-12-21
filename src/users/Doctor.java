2497937d4e5a064db9b695614f6f0973f8cc68d5
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-21 16:49:01 +0000

    Update van scheduler: houdt rekening met de working hours van de nurse.

--- src/users/Doctor.java
+++ src/users/Doctor.java
@@ -1,7 +1,10 @@
 package users;
 
+import scheduler.HospitalDate;
+import scheduler.TimeSlot;
 import controllers.interfaces.DoctorIN;
 import exceptions.InvalidNameException;
+import exceptions.InvalidSchedulingRequestException;
 import exceptions.InvalidTimeSlotException;
 
 public class Doctor extends SchedulableUser implements DoctorIN
@@ -9,6 +12,11 @@ public class Doctor extends SchedulableUser implements DoctorIN
 	public Doctor(String name) throws InvalidNameException, InvalidTimeSlotException {
 		super(name);
 	}
-	
+
+	@Override
+	public TimeSlot getFirstFreeSlotBetween(HospitalDate startDate,
+			HospitalDate stopDate, long duration) throws InvalidSchedulingRequestException, InvalidTimeSlotException {
+		return this.getTimeTable().getFirstFreeSlotBetween(startDate, stopDate, duration);
+	}
 	
 }
--- src/users/Nurse.java
+++ src/users/Nurse.java
@@ -1,6 +1,7 @@
 package users;
 
 import scheduler.HospitalDate;
+import scheduler.TimeSlot;
 import exceptions.InvalidNameException;
 import exceptions.InvalidSchedulingRequestException;
 import exceptions.InvalidTimeSlotException;
@@ -13,6 +14,9 @@ public class Nurse extends SchedulableUser
 
 	// private HospitalDate startsWorkAt = new HospitalDate(0, 0, 0, 8, 0, 0);
 	// private HospitalDate stopsWorkAt = new HospitalDate(0, 0, 0, 17, 0, 0);
+
+	private static final int STARTHOUR = 8;
+	private static final int STOPHOUR = 17;
 
 	/**
 	 * Default constructor. Will initialise all fields.
@@ -38,11 +42,49 @@ public class Nurse extends SchedulableUser
 	public boolean canBeScheduledOn(HospitalDate startDate,
 			HospitalDate stopDate) throws InvalidSchedulingRequestException,
 			InvalidTimeSlotException {
-//		return startDate.getHour() >= this.startsWorkAt.getHour()
-//				&& startDate.getHour() < this.stopsWorkAt.getHour()
-//				&& stopDate.getHour() > this.startsWorkAt.getHour()
-//				&& stopDate.getHour() <= this.stopsWorkAt.getHour()
-//				&& this.timeTable.hasFreeSlotAt(startDate, stopDate);
-		return this.timeTable.hasFreeSlotAt(startDate,stopDate);
+		return this.timeTable.hasFreeSlotAt(startDate, stopDate);
+	}
+
+	@Override
+	public TimeSlot getFirstFreeSlotBetween(HospitalDate startDate,
+			HospitalDate stopDate, long duration)
+			throws InvalidSchedulingRequestException, InvalidTimeSlotException {
+		HospitalDate tmpStartWorkingHour = new HospitalDate(0, 0, 0,
+				Nurse.STARTHOUR, 0, 0);
+		HospitalDate tmpStopWorkingHour = new HospitalDate(0, 0, 0,
+				Nurse.STOPHOUR, 0, 0);
+		HospitalDate tmpStartDate = this.createDummyDate(startDate);
+		HospitalDate tmpStopDate = this.createDummyDate(stopDate);
+		if (tmpStartDate.before(tmpStartWorkingHour)) {
+			startDate = new HospitalDate(startDate.getYear(),
+					startDate.getMonth(), startDate.getDay(), Nurse.STARTHOUR,
+					0, 0);
+		} else if (tmpStopWorkingHour.before(tmpStartDate)
+				|| tmpStopWorkingHour.equals(tmpStartDate)) {
+			startDate = new HospitalDate(startDate.getYear(),
+					startDate.getMonth(), startDate.getDay() + 1,
+					Nurse.STARTHOUR, 0, 0);
+		}
+		if (tmpStopDate.before(tmpStartWorkingHour)
+				|| tmpStopDate.equals(tmpStartWorkingHour)) {
+			stopDate = new HospitalDate(stopDate.getYear(),
+					stopDate.getMonth(), stopDate.getDay() - 1,
+					Nurse.STARTHOUR, 0, 0);
+		} else if (tmpStopWorkingHour.before(tmpStopDate)) {
+			stopDate = new HospitalDate(stopDate.getYear(),
+					stopDate.getMonth(), stopDate.getDay(), Nurse.STARTHOUR, 0,
+					0);
+		}
+		if (stopDate.before(startDate)) {
+			throw new InvalidSchedulingRequestException(
+					"No more free slots available between the given Start -and EndDate!");
+		}
+		return this.getTimeTable().getFirstFreeSlotBetween(startDate, stopDate,
+				duration);
+	}
+
+	private HospitalDate createDummyDate(HospitalDate hospitalDate) {
+		return new HospitalDate(0, 0, 0, hospitalDate.getHour(),
+				hospitalDate.getMinute(), hospitalDate.getSecond());
 	}
 }
