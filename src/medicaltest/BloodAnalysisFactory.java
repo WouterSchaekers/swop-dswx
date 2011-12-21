d79aaf615227c9948fb7e02fafa23d61dfc2e137
dietergeboers <dietergeboers@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-21 16:10:47 +0000

    Some Tests for the factories added

--- src/medicaltest/BloodAnalysisFactory.java
+++ src/medicaltest/BloodAnalysisFactory.java
@@ -1,36 +1,91 @@
 package medicaltest;
 
+import exceptions.InvalidDurationException;
+import exceptions.InvalidNameException;
+import exceptions.InvalidTimeSlotException;
+
 
 public class BloodAnalysisFactory extends MedicalTestFactory
 {
-	
+	 private String focus;
+	private int numberOfAnalysis;
+	private int duration;
+	BloodAnalysisFactory() {
+		setDuration(45);}
+	/**
+	 * Method to set the focus of the bloodanalysis
+	 * @param focus
+	 * @throws IllegalArgumentException if the provided argument is null
+	 */
 	public void setFocus(String focus) {
 		if(inValidFocus(focus))
 			throw new IllegalArgumentException("invalid focus value");
 		this.focus = focus;
 	}
-
-	private boolean inValidFocus(String focus2) {
-		return focus!=null;
-	}
-	
+	/**
+	 * Method to set the number of analysis that have to be executed
+	 * @param numberOfAnalysis
+	 * @throws if the provided number is invalid (<=0)
+	 */
 	public void setNumberOfAnalysis(int numberOfAnalysis) {
 		if(inValidNumberOfAnalysis(numberOfAnalysis))
 			throw new IllegalArgumentException("Illegal amount of analysis");
 		this.numberOfAnalysis = numberOfAnalysis;
 	}
-	
-	private boolean inValidNumberOfAnalysis(int numberOfAnalysis2) {
-		return numberOfAnalysis2>0;
+	@Override
+	public MedicalTest create() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException {
+		if(!ready())
+			throw new IllegalArgumentException();
+		
+		return new BloodAnalysis(numberOfAnalysis, focus,duration);
 	}
-	public void setDuration(int duration) {
+	/**
+	 * Checks all the set fields in this factory.
+	 * @return
+	 *  false if one of the fields has not been inialized or is wrongfully inialized.
+	 */
+	private boolean ready() {
+		boolean rv = true;
+		rv	&=	!inValidDuration(duration);
+		rv	&=	!inValidFocus(focus);
+		rv  &=	!inValidNumberOfAnalysis(numberOfAnalysis);
+		return rv;
+	}
+	/**
+	 * Simple method to check if the provided focus argument is valid
+	 * @param focus
+	 * 	the focus of the bloodanalysis that will be created by this factory
+	 * @return
+	 * 	true if the argumet is  null.
+	 */
+	private boolean inValidFocus(String focus) {
+		return focus==null;
+	}
+	/**
+	 * Method to check if the amount of analysis is invalid ( less then 0)
+	 * @param numberOfAnalysis
+	 * the number of analysis that have to be executed
+	 * @return
+	 * 	true if the argument is <= 0
+	 */
+	private boolean inValidNumberOfAnalysis(int numberOfAnalysis) {
+		return numberOfAnalysis<=0;
+	}
+	/**
+	 * private setter to maybe use in future iteration
+	 * @param duration
+	 */
+	private void setDuration(int duration) {
+		if(inValidDuration(duration))
+			throw new IllegalArgumentException("wrong duration provided");
 		this.duration = duration;
 	}
-	private String focus;
-	private int numberOfAnalysis;
-	private int duration;
-	@Override
-	public MedicalTest create() {
-		return null;
+	/**
+	 * Checks if the duration is invalid
+	 * @param duration2
+	 * @return
+	 */
+	private boolean inValidDuration(int duration2) {
+		return duration2!=45;
 	}
 }
---
+++ src/medicaltest/FactoryTestsMedicalTests.java
@@ -0,0 +1,170 @@
+package medicaltest;
+
+import static org.junit.Assert.*;
+import java.util.Collection;
+import org.junit.Test;
+import exceptions.InvalidDurationException;
+import exceptions.InvalidNameException;
+import exceptions.InvalidTimeSlotException;
+
+public class FactoryTestsMedicalTests
+{
+
+	public static final UltraSoundScanFactory filterUltraSoundFactory(Collection<MedicalTestFactory> f)
+	{
+		for(MedicalTestFactory s:f)
+			if(s instanceof UltraSoundScanFactory)
+				return (UltraSoundScanFactory)s;
+		throw new IllegalArgumentException();
+	}
+	public static final XRayScanFactory filterXrayScan(Collection<MedicalTestFactory> f)
+	{
+		for(MedicalTestFactory s:f)
+			if(s instanceof XRayScanFactory)
+				return (XRayScanFactory)s;
+		throw new IllegalArgumentException();
+	}
+	public static final BloodAnalysisFactory filterBloodAnalysis(Collection<MedicalTestFactory> f)
+	{
+		for(MedicalTestFactory s:f)
+			if(s instanceof BloodAnalysisFactory)
+				return (BloodAnalysisFactory)s;
+		throw new IllegalArgumentException();
+	}
+	
+	MedicalTests m = new MedicalTests();
+	@Test
+	public void createUltraSoundScan1() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		UltraSoundScanFactory fac= filterUltraSoundFactory(m.factories());
+		fac.setRecordImages(true);
+		fac.setRecordVid(true);
+		fac.setScanInfo("Scaninfo");
+		UltraSoundScan scan = (UltraSoundScan) fac.create();
+		assertTrue(scan.getScaninfo().equals("Scaninfo"));
+		assertTrue(scan.hasImageRecordingEnabled());
+		assertTrue(scan.hasVideoRecordingEnabled());
+		
+	}
+	@Test
+	public void createUltraSoundScan2() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		UltraSoundScanFactory fac= filterUltraSoundFactory(m.factories());
+		fac.setRecordImages(false);
+		fac.setRecordVid(true);
+		fac.setScanInfo("Scaninfo");
+		UltraSoundScan scan = (UltraSoundScan) fac.create();
+		assertTrue(scan.getScaninfo().equals("Scaninfo"));
+		assertTrue(scan.hasImageRecordingEnabled()==false);
+		assertTrue(scan.hasVideoRecordingEnabled());
+		
+	}
+	@Test
+	public void createUltraSoundScan() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		UltraSoundScanFactory fac= filterUltraSoundFactory(m.factories());
+		fac.setRecordImages(true);
+		fac.setRecordVid(false);
+		fac.setScanInfo("Scaninfo");
+		UltraSoundScan scan = (UltraSoundScan) fac.create();
+		assertTrue(scan.getScaninfo().equals("Scaninfo"));
+		assertTrue(scan.hasImageRecordingEnabled());
+		assertTrue(scan.hasVideoRecordingEnabled()==false);
+		
+	}
+	@Test
+	public void createUltraSoundScan3() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		UltraSoundScanFactory fac= filterUltraSoundFactory(m.factories());
+		fac.setRecordImages(false);
+		fac.setRecordVid(false);
+		fac.setScanInfo("Scaninfo");
+		UltraSoundScan scan = (UltraSoundScan) fac.create();
+		assertTrue(scan.getScaninfo().equals("Scaninfo"));
+		assertTrue(scan.hasImageRecordingEnabled()==false);
+		assertTrue(scan.hasVideoRecordingEnabled()==false);
+		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void createUltraSoundScanError1() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		UltraSoundScanFactory fac= filterUltraSoundFactory(m.factories());
+		fac.create();
+		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void createUltraSoundScanError2() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		UltraSoundScanFactory fac= filterUltraSoundFactory(m.factories());
+		fac.setRecordImages(true);
+		fac.create();
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void createUltraSoundScanError3() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		UltraSoundScanFactory fac= filterUltraSoundFactory(m.factories());
+		fac.setRecordImages(true);
+		fac.setRecordVid(true);
+		fac.create();
+	}
+	@Test(expected=IllegalArgumentException.class)
+	public void createUltraSoundScanError4() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		UltraSoundScanFactory fac= filterUltraSoundFactory(m.factories());
+		fac.setRecordImages(true);
+		fac.setRecordVid(true);
+		fac.setScanInfo(null);
+		fac.create();
+	}
+	@Test
+	public void createBloodAnalysisTest1() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		BloodAnalysisFactory fac= filterBloodAnalysis(m.factories());
+		fac.setFocus("focus");
+		fac.setNumberOfAnalysis(3);
+		BloodAnalysis scan = (BloodAnalysis) fac.create();
+		assertTrue(scan.getAmount()==3);
+		assertTrue(scan.getFocus().equals("focus"));
+		
+	}
+	@Test
+	public void createBloodAnalysisTest2() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		BloodAnalysisFactory fac= filterBloodAnalysis(m.factories());
+		fac.setFocus("focus");
+		fac.setNumberOfAnalysis(4);
+		BloodAnalysis scan = (BloodAnalysis) fac.create();
+		assertTrue(scan.getAmount()==4);
+		assertTrue(scan.getFocus().equals("focus"));
+		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void createBloodAnalysisTesterror1() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		BloodAnalysisFactory fac= filterBloodAnalysis(m.factories());
+		fac.setFocus("focus");
+		fac.setNumberOfAnalysis(0);
+		fac.create();		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void createBloodAnalysisTesterror2() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		BloodAnalysisFactory fac= filterBloodAnalysis(m.factories());
+		fac.create();		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void createBloodAnalysisTesterror3() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		BloodAnalysisFactory fac= filterBloodAnalysis(m.factories());
+		fac.setFocus("f");
+		fac.create();		
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void createBloodAnalysisTesterror4() throws InvalidNameException, InvalidDurationException, InvalidTimeSlotException
+	{
+		BloodAnalysisFactory fac= filterBloodAnalysis(m.factories());
+		fac.setNumberOfAnalysis(4);
+		fac.create();		
+	}
+
+}
