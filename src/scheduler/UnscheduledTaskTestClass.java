49b11d975d37d2e2492b4e56b312f713eb2bb22f
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-28 17:20:37 +0000

    UnscheduledTaskTestClass updated.

--- src/scheduler/UnscheduledTaskTestClass.java
+++ src/scheduler/UnscheduledTaskTestClass.java
@@ -1,62 +1,49 @@
 package scheduler;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.LinkedList;
+import scheduler.task.Schedulable;
+import scheduler.task.unscheduled.UnscheduledTask;
 import exceptions.InvalidAmountException;
 import exceptions.InvalidDurationException;
 import exceptions.InvalidHospitalDateException;
 import exceptions.InvalidOccurencesException;
 import exceptions.InvalidResourceException;
-import scheduler.task.Schedulable;
-import scheduler.task.unscheduled.UnscheduledTask;
+
 /**
- * Class created for testing purposes only !!!F!!!!!!!!!@!$!Q@#$!@#$
- *
+ * Class created for testing purposes only!
+ * DO NOT USE THIS!
  */
 public class UnscheduledTaskTestClass extends UnscheduledTask
 {
 
-	private LinkedList<LinkedList<Schedulable>> s;
-	private LinkedList<Integer> a;
+	private LinkedList<Schedulable> requirements;
+	private LinkedList<LinkedList<Schedulable>> schedulables;
+	private LinkedList<Integer> occurences;
 
 	public UnscheduledTaskTestClass(long duration, HospitalDate creationTime,
-			Collection<Schedulable> requirements, long extraTime, boolean backToBack, LinkedList<LinkedList<Schedulable>> s,LinkedList<Integer> occ)
-			throws InvalidResourceException, InvalidDurationException,
-			InvalidOccurencesException, InvalidAmountException,
-			InvalidHospitalDateException {
+			long extraTime, LinkedList<Schedulable> r,
+			LinkedList<LinkedList<Schedulable>> s, LinkedList<Integer> occ,
+			boolean backToBack) throws InvalidResourceException,
+			InvalidDurationException, InvalidOccurencesException,
+			InvalidAmountException, InvalidHospitalDateException {
 		super(null, duration, creationTime, extraTime, backToBack);
-		this.s=s;
-		this.a=occ;
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-	@Override
-	public long getExtraTime() {
-		// TODO Auto-generated method stub
-		return 0;
+		this.requirements = r;
+		this.schedulables = s;
+		this.occurences = occ;
 	}
 
 	@Override
 	public LinkedList<LinkedList<Schedulable>> getResourcePool() {
-		// TODO Auto-generated method stub
-		return s;
+		return schedulables;
 	}
 
 	@Override
 	public LinkedList<Integer> getOccurences() {
-		// TODO Auto-generated method stub
-		return a;
+		return occurences;
 	}
-
-
 
 	@Override
 	public boolean canBeScheduled() {
-		// TODO Auto-generated method stub
 		return false;
 	}
-
 }
