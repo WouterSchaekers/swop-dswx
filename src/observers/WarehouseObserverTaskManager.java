54f59be8b50a5f27bd6d2a32c2ec67c2bfd5c601
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-29 22:31:35 +0000

    WarehouseObserverTaskManager fixed.

---
+++ src/observers/WarehouseObserverTaskManager.java
@@ -0,0 +1,19 @@
+package observers;
+
+import java.util.Observable;
+import java.util.Observer;
+import scheduler.task.TaskManager;
+
+public class WarehouseObserverTaskManager implements Observer
+{
+	TaskManager taskManager;
+	
+	public WarehouseObserverTaskManager(TaskManager taskManager){
+		this.taskManager = taskManager;
+	}
+	@Override
+	public void update(Observable o, Object arg) {
+		taskManager.update();
+	}
+
+}
