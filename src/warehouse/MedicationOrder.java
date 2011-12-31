8261d77fbf975ffcc4d3a171b1dbbfdfb5378a67
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-31 08:20:57 +0000

    Update of WarehouseAdmin and MedicationOrder.

--- src/warehouse/MedicationOrder.java
+++ src/warehouse/MedicationOrder.java
@@ -2,7 +2,6 @@ package warehouse;
 
 import scheduler.HospitalDate;
 import treatment.Medication;
-import users.HospitalAdmin;
 import users.WarehouseAdmin;
 
 public class MedicationOrder extends StockOrder
@@ -10,26 +9,45 @@ public class MedicationOrder extends StockOrder
 	private final HospitalDate orderDate;
 	private final MedicationType medicationType;
 	private final WarehouseAdmin warehouseAdmin;
-	
-	public MedicationOrder(HospitalDate orderDate, WarehouseAdmin warehouseAdmin, MedicationType medicationType){
+
+	public MedicationOrder(HospitalDate orderDate,
+			WarehouseAdmin warehouseAdmin, MedicationType medicationType) {
 		this.orderDate = orderDate;
 		this.warehouseAdmin = warehouseAdmin;
 		this.medicationType = medicationType;
 	}
-	
+
 	@Override
-	public HospitalDate getOrderDate(){
+	public HospitalDate getOrderDate() {
 		return new HospitalDate(this.orderDate);
 	}
-	
+
 	@Override
-	public WarehouseAdmin getWarehouseAdmin(){
+	public WarehouseAdmin getWarehouseAdmin() {
 		return this.warehouseAdmin;
 	}
 
 	@Override
 	public Medication getStockItem() {
-		return new Medication(medicationType.toString(), false, new HospitalDate(orderDate.getTimeSinceStart() + this.TIME_FOR_EXPIRATION), medicationType)
-		{};
+		if (medicationType.equals(new ActivatedCarbon(false, null)))
+			return new ActivatedCarbon(false, new HospitalDate(
+					this.orderDate.getTimeSinceStart()
+							+ this.TIME_FOR_EXPIRATION));
+		else if (medicationType.equals(new Aspirin(false, null)))
+			return new Aspirin(false, new HospitalDate(
+					this.orderDate.getTimeSinceStart()
+							+ this.TIME_FOR_EXPIRATION));
+		else if (medicationType.equals(new Misc(false, null)))
+			return new Misc(false, new HospitalDate(
+					this.orderDate.getTimeSinceStart()
+							+ this.TIME_FOR_EXPIRATION));
+		else if (medicationType.equals(new SleepingTablets(false, null)))
+			return new SleepingTablets(false, new HospitalDate(
+					this.orderDate.getTimeSinceStart()
+							+ this.TIME_FOR_EXPIRATION));
+		else
+			return new Vitamins(false, new HospitalDate(
+					this.orderDate.getTimeSinceStart()
+							+ this.TIME_FOR_EXPIRATION));
 	}
 }
