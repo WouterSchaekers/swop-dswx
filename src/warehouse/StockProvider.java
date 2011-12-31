ae71f2698eb849cf836290feae3352d02ca3d442
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-31 10:07:52 +0000

    Observers are almost fixed.

--- src/warehouse/StockProvider.java
+++ src/warehouse/StockProvider.java
@@ -1,22 +1,26 @@
 package warehouse;
 
 import java.util.LinkedList;
+import exceptions.WarehouseException;
+import exceptions.WarehouseOverCapacityException;
 import scheduler.HospitalDate;
-import users.HospitalAdmin;
 import users.WarehouseAdmin;
 
 public class StockProvider
 {
-	LinkedList<PlasterOrder> orderedPlaster;
-	LinkedList<MedicationOrder> orderedMedication;
-	LinkedList<MealOrder> orderedMeals;
-	HospitalDate curDate;
+	private LinkedList<PlasterOrder> orderedPlaster;
+	private LinkedList<MedicationOrder> orderedMedication;
+	private LinkedList<MealOrder> orderedMeals;
+	private HospitalDate curDate;
+	private int extraHour;
+	
 	
 	public StockProvider(){
 		this.orderedPlaster = new LinkedList<PlasterOrder>();
 		this.orderedMedication = new LinkedList<MedicationOrder>();
 		this.orderedMeals = new LinkedList<MealOrder>();
 		this.curDate = new HospitalDate();
+		this.extraHour = 2;
 	}
 	
 	public void orderPlaster(WarehouseAdmin warehouseAdmin){
@@ -29,5 +33,49 @@ public class StockProvider
 	
 	public void orderMeal(WarehouseAdmin warehouseAdmin){
 		this.orderedMeals.add(new MealOrder(this.curDate, warehouseAdmin));
+	}
+	
+	public void updateTime(HospitalDate hospitalDate) throws WarehouseException, WarehouseOverCapacityException{
+		this.updatePlaster(hospitalDate);
+		this.updateMedication(hospitalDate);
+		this.updateMeals(hospitalDate);
+	}
+	
+	private void updatePlaster(HospitalDate hospitalDate) throws WarehouseException{
+		for(int i = 0; i < orderedPlaster.size(); i++){
+			PlasterOrder curPlasterOrder = orderedPlaster.get(i);
+			if(this.mayBeReleased(this.orderedPlaster.get(i), hospitalDate)){
+				WarehouseAdmin warehouseAdmin = curPlasterOrder.getWarehouseAdmin();
+				warehouseAdmin.addPlasterOrder(curPlasterOrder);
+			}
+		}
+	}
+	
+	private void updateMedication(HospitalDate hospitalDate) throws WarehouseException{
+		for(int i = 0; i < orderedMedication.size(); i++){
+			MedicationOrder curMedicationOrder = orderedMedication.get(i);
+			if(this.mayBeReleased(this.orderedMedication.get(i), hospitalDate)){
+				WarehouseAdmin warehouseAdmin = curMedicationOrder.getWarehouseAdmin();
+				warehouseAdmin.addMedicationOrder(curMedicationOrder);
+			}
+		}
+	}
+	
+	private void updateMeals(HospitalDate hospitalDate) throws WarehouseException, WarehouseOverCapacityException{
+		for(int i = 0; i < orderedMeals.size(); i++){
+			MealOrder curMealOrder = orderedMeals.get(i);
+			if(this.mayBeReleased(this.orderedMeals.get(i), hospitalDate)){
+				WarehouseAdmin warehouseAdmin = curMealOrder.getWarehouseAdmin();
+				warehouseAdmin.addMealOrder(curMealOrder);
+			}
+		}
+	}
+	
+	private boolean mayBeReleased(StockOrder stockOrder, HospitalDate hospitalDate){
+		HospitalDate orderedDate = stockOrder.getOrderDate();
+		if(!hospitalDate.before(new HospitalDate(orderedDate.getYear(), orderedDate.getMonth(), orderedDate.getDay() + this.extraHour, 6, 0, 0))){
+			return true;
+		}
+		return false;
 	}
 }
