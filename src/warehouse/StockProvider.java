36d741edaa933ee855f601fa7e1dffbda592af7e
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2011-12-31 07:58:43 +0000

    Updated StockOrders.

--- src/warehouse/StockProvider.java
+++ src/warehouse/StockProvider.java
@@ -1,21 +1,33 @@
 package warehouse;
 
 import java.util.LinkedList;
+import scheduler.HospitalDate;
 import users.HospitalAdmin;
+import users.WarehouseAdmin;
 
 public class StockProvider
 {
 	LinkedList<PlasterOrder> orderedPlaster;
 	LinkedList<MedicationOrder> orderedMedication;
 	LinkedList<MealOrder> orderedMeals;
+	HospitalDate curDate;
 	
 	public StockProvider(){
 		this.orderedPlaster = new LinkedList<PlasterOrder>();
 		this.orderedMedication = new LinkedList<MedicationOrder>();
 		this.orderedMeals = new LinkedList<MealOrder>();
+		this.curDate = new HospitalDate();
 	}
 	
-	public void orderPlaster(HospitalAdmin hospitalAdmin){
-		
+	public void orderPlaster(WarehouseAdmin warehouseAdmin){
+		this.orderedPlaster.add(new PlasterOrder(this.curDate, warehouseAdmin));
+	}
+	
+	public void orderMedication(WarehouseAdmin warehouseAdmin, MedicationType medicationType){
+		this.orderedMedication.add(new MedicationOrder(this.curDate, warehouseAdmin, medicationType));
+	}
+	
+	public void orderMeal(WarehouseAdmin warehouseAdmin){
+		this.orderedMeals.add(new MealOrder(this.curDate, warehouseAdmin));
 	}
 }
