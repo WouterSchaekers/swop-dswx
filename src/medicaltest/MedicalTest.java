e3c7dea12d85d699165e7f854d4daf24f1fcaf1b
vegetas.fate@gmail.com <vegetas.fate@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-02-29 11:02:13 +0000

    same as previous

--- src/medicaltest/MedicalTest.java
+++ src/medicaltest/MedicalTest.java
@@ -43,7 +43,7 @@ public abstract class MedicalTest implements MedicalTestIN
 	 *             If(!isValidDuration(duration))
 	 * @throws InvalidTimeSlotException
 	 */
-	public MedicalTest(long duration) throws InvalidDurationException, InvalidTimeSlotException {
+	public MedicalTest(long duration) throws InvalidDurationException {
 
 		if (!isValidDuration(duration))
 			throw new InvalidDurationException(
