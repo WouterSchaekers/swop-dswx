01af0ef1c1eb9c2699985a488b5b04613b29024d
vegetas.fate@gmail.com <vegetas.fate@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-03-02 10:50:31 +0000

    autofix ^^

--- src/controllers/AddHospitalStaffController.java
+++ src/controllers/AddHospitalStaffController.java
@@ -1,9 +1,19 @@
 package controllers;
 
+import exceptions.InvalidHospitalStateException;
+import exceptions.InvalidLoginControllerException;
+import system.HospitalState;
 import users.User;
 
 public class AddHospitalStaffController extends NeedsLoginController
 {
+
+	public AddHospitalStaffController(HospitalState hospitalState,
+			LoginController controller) throws InvalidLoginControllerException,
+			InvalidHospitalStateException {
+		super(hospitalState, controller);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	boolean validUser(User u) {
