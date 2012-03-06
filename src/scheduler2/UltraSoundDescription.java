67a3f871f3c15e02376541d17f5bee7a242f4acb
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-03-06 15:39:01 +0000

    Add all the description!

---
+++ src/scheduler2/UltraSoundDescription.java
@@ -0,0 +1,20 @@
+package scheduler2;
+
+import java.util.Collection;
+
+public class UltraSoundDescription extends TaskDescription
+{
+
+	@Override
+	public Collection<Requirement> getAllRequireMents() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isReady() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
