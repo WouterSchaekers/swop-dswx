3350fbfe12276ea19cc3b98d02c33f0b926c035c
vegetas.fate@gmail.com <vegetas.fate@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-03-06 14:44:54 +0000

    Warehouse klasses aanmaken

---
+++ src/warehouse/item/ActivatedCarbonType.java
@@ -0,0 +1,14 @@
+package warehouse.item;
+
+import scheduler.HospitalDate;
+
+public class ActivatedCarbonType extends WarehouseItemType
+{
+
+	@Override
+	public WarehouseItem create(HospitalDate expirydate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/warehouse/item/SleepingTabletsType.java
@@ -0,0 +1,14 @@
+package warehouse.item;
+
+import scheduler.HospitalDate;
+
+public class SleepingTabletsType extends WarehouseItemType
+{
+
+	@Override
+	public WarehouseItem create(HospitalDate expirydate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
