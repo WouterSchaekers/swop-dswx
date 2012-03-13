2f597f7adc6993fffd0cf3c0b3ff2e99ca4f3674
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-03-13 15:06:17 +0000

    UserManagers++

--- src/system/Campus.java
+++ src/system/Campus.java
@@ -9,8 +9,37 @@ public class Campus
 	private UserManager _usm = new UserManager();
 	private MachinePool _mp = new MachinePool();
 	private Warehouse _wh = new Warehouse(); 
+	Hospital _h = new Hospital();
 	
 	public UserManager getUserManager(){
 		return _usm;
+	}
+	
+	public void setUserManager(UserManager userManager){
+		this._usm = userManager;
+	}
+	
+	public Hospital getHospital(){
+		return this._h;
+	}
+	
+	public void setHospital(Hospital hospital){
+		this._h = hospital;
+	}
+	
+	public Warehouse getWarehouse(){
+		return this._wh;
+	}
+	
+	public void setWarehouse(Warehouse warehouse){
+		this._wh = warehouse;
+	}
+	
+	public MachinePool getMachinePool(){
+		return this._mp;
+	}
+	
+	public void setMachinePool(MachinePool machinePool){
+		this._mp = machinePool;
 	}
 }
