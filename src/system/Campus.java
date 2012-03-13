f14b2978795f144ea5b450afe252e0a70f2a060d
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-03-13 12:38:09 +0000

    Abracommit.

--- src/system/Campus.java
+++ src/system/Campus.java
@@ -1,6 +1,16 @@
 package system;
 
+import machine.MachinePool;
+import users.UserManager;
+import warehouse.Warehouse;
+
 public class Campus
 {
+	UserManager _usm = new UserManager();
+	MachinePool _mp = new MachinePool();
+	Warehouse _wh = new Warehouse();
 	
+	public UserManager getUserManager(){
+		return _usm;
+	}
 }
