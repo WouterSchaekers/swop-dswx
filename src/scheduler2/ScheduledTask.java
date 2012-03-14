6b6720ee73301a5574b3e9d855ca19a6e0d7e885
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-03-14 21:46:55 +0000

    Implemented ScheduledTask.

--- src/scheduler2/ScheduledTask.java
+++ src/scheduler2/ScheduledTask.java
@@ -6,24 +6,28 @@ import scheduler.task.Schedulable;
 
 public class ScheduledTask extends Task
 {
+	Collection<Schedulable> _resources;
+	HospitalDate _startDate;
+	HospitalDate _endDate;
 
-	ScheduledTask(TaskDescription description) 
-	{
+	ScheduledTask(TaskDescription description,
+			Collection<Schedulable> resources, HospitalDate startDate,
+			HospitalDate endDate) {
 		super(description);
+		this._resources = resources;
+		this._startDate = startDate;
+		this._endDate = endDate;
 	}
-	public Collection<Schedulable> getUsedResources()
-	{
-		return null;
-		
+
+	public Collection<Schedulable> getUsedResources() {
+		return this._resources;
 	}
-	public HospitalDate getStartDate()
-	{
-		return null;
+
+	public HospitalDate getStartDate() {
+		return this._startDate;
 	}
-	public HospitalDate getEndDate()
-	{
-		return null;
+
+	public HospitalDate getEndDate() {
+		return this._endDate;
 	}
-	
-	
 }
