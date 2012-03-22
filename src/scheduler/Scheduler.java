44decbefaace79a3eafc8a7482160ab493fe4827
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-03-22 01:49:07 +0000

    getAvailableSchedulables is af.

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -21,14 +21,33 @@ public class Scheduler
 		LinkedList<LinkedList<Schedulable>> availableSchedulables = this.getAvailableSchedulables(schedulablePool, requirements);
 		HospitalDate minimumDate = new HospitalDate(description.getCreationTime().getTimeSinceStart() + description.getExtraTime());
 		HospitalDate stopDate = new HospitalDate(HospitalDate.END_OF_TIME);
-		return schedule(availableSchedulables, HospitalDate.getMaximum(currentDate, minimumDate), stopDate, description.getDuration());
+		return schedule(availableSchedulables, 0, HospitalDate.getMaximum(currentDate, minimumDate), stopDate, description.getDuration());
 	}
 	
-	private ScheduledTask schedule(LinkedList<LinkedList<Schedulable>> availableSchedulables, HospitalDate startDate, HospitalDate stopDate, long duration){
+	private ScheduledTask schedule(LinkedList<LinkedList<Schedulable>> availableSchedulables, int iteration, HospitalDate startDate, HospitalDate stopDate, long duration){
 		return null;
 	}
 	
 	private LinkedList<LinkedList<Schedulable>> getAvailableSchedulables(Collection<Schedulable> schedulablePool, Collection<Requirement> requirements) throws IsNotReadyException{
-		return null;
+		Collection<Requirement> notMetYet = new LinkedList<Requirement>();
+		for(Requirement requirement : requirements){
+			if(!requirement.isMet()){
+				notMetYet.add(requirement);
+			}
+		}
+		LinkedList<LinkedList<Schedulable>> availableSchedulables = new LinkedList<LinkedList<Schedulable>>();
+		for(Requirement requirement : notMetYet){
+			LinkedList<Schedulable> isMetBy = new LinkedList<Schedulable>();
+			for(Schedulable schedulable : schedulablePool){
+				if(requirement.isMetBy(schedulable)){
+					isMetBy.add(schedulable);
+				}
+			}
+			if(isMetBy.size() == 0){
+				throw new IsNotReadyException("Some of the requirements are not ready yet.");
+			}
+			availableSchedulables.add(isMetBy);
+		}
+		return availableSchedulables;
 	}
 }
