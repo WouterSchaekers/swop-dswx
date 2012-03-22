10c638a186823d6d93fbb1423c341ae9325b0398
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-03-22 11:24:08 +0000

    Nog meer scheduler!

--- src/scheduler/Scheduler.java
+++ src/scheduler/Scheduler.java
@@ -3,17 +3,17 @@ package scheduler;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
-import exceptions.ConditionNotMetException;
 import scheduler.requirements.Requirement;
 import scheduler.tasks.ScheduledTask;
 import scheduler.tasks.TaskDescription;
 import scheduler.tasks.UnscheduledTask;
+import exceptions.InvalidSchedulingRequestException;
 
 public class Scheduler
 {
 	public Scheduler(){}
 	
-	public ScheduledTask schedule(SchedulingData schedulingDate) throws ConditionNotMetException{
+	public ScheduledTask schedule(SchedulingData schedulingDate) throws InvalidSchedulingRequestException{
 		Collection<Schedulable> schedulablePool = schedulingDate.getAllSchedulables();
 		HospitalDate currentDate = schedulingDate.getTimeLord().getSystemTime();
 		UnscheduledTask unscheduledTask = schedulingDate.getUnscheduledTask();
@@ -24,10 +24,13 @@ public class Scheduler
 		HospitalDate stopDate = new HospitalDate(HospitalDate.END_OF_TIME);
 		Collection<Requirement> metRequirements = getMetRequirements(schedulablePool, requirements, startDate);
 		HashMap<LinkedList<Schedulable>, Integer> availableSchedulables = this.getAvailableSchedulables(schedulablePool, requirements, metRequirements);
-		return schedule(availableSchedulables, 0, HospitalDate.getMaximum(currentDate, minimumDate), stopDate, description.getDuration());
+		this.removeDoubleBookings(availableSchedulables);
+		this.checkIfEnoughResources(availableSchedulables);
+		return schedule(availableSchedulables, 0, startDate, stopDate, description.getDuration());
 	}
 	
 	private ScheduledTask schedule(HashMap<LinkedList<Schedulable>, Integer> availableSchedulables, int iteration, HospitalDate startDate, HospitalDate stopDate, long duration){
+		
 		return null;
 	}
 	
@@ -41,7 +44,7 @@ public class Scheduler
 		return isAlreadyMet;
 	}
 	
-	private HashMap<LinkedList<Schedulable>, Integer> getAvailableSchedulables(Collection<Schedulable> schedulablePool, Collection<Requirement> requirements, Collection<Requirement> metRequirements) throws ConditionNotMetException{
+	private HashMap<LinkedList<Schedulable>, Integer> getAvailableSchedulables(Collection<Schedulable> schedulablePool, Collection<Requirement> requirements, Collection<Requirement> metRequirements) throws InvalidSchedulingRequestException{
 		Collection<Requirement> notMetYet = new LinkedList<Requirement>();
 		for(Requirement requirement : requirements){
 			if(!metRequirements.contains(requirement)){
@@ -57,10 +60,28 @@ public class Scheduler
 				}
 			}
 			if(isMetBy.size() == 0){
-				throw new ConditionNotMetException("Some of the necessary conditions were not satisfied.");
+				throw new InvalidSchedulingRequestException("Some of the necessary conditions were not satisfied.");
 			}
-			availableSchedulables.add(isMetBy);
+			availableSchedulables.put(isMetBy, requirement.getAmount());
 		}
 		return availableSchedulables;
+	}
+	
+	private void removeDoubleBookings(HashMap<LinkedList<Schedulable>, Integer> availableSchedulables){
+		for(LinkedList<Schedulable> schedulablePool : availableSchedulables.keySet()){
+			for(LinkedList<Schedulable> curSchedulablePool : availableSchedulables.keySet()){
+				if(curSchedulablePool.size() == 1){
+					schedulablePool.remove(curSchedulablePool.get(0));
+				}
+			}
+		}
+	}
+	
+	private void checkIfEnoughResources(HashMap<LinkedList<Schedulable>, Integer> availableSchedulables) throws InvalidSchedulingRequestException{
+		for(LinkedList<Schedulable> schedulablePool : availableSchedulables.keySet()){
+			if(schedulablePool.size() < availableSchedulables.get(schedulablePool)){
+				throw new InvalidSchedulingRequestException("There are not enough resources to schedule this task.");
+			}
+		}
 	}
 }
