5f7cd39e0414c565b2f6b64ad0086ad28a863002
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-03-23 12:46:07 +0000

    Improvement of the scheduler.

--- src/scheduler/SchedulingData.java
+++ src/scheduler/SchedulingData.java
@@ -2,6 +2,7 @@ package scheduler;
 
 import java.util.Collection;
 import scheduler.tasks.UnscheduledTask;
+import system.Location;
 
 /**
  * Use this object to transfer a bunch of data to the scheduler when you want to
@@ -10,12 +11,14 @@ import scheduler.tasks.UnscheduledTask;
 public class SchedulingData
 {
 	private Collection<Schedulable> allSchedulables_;
+	private Collection<Location> locations_;
 	private TimeLord timeLord_;
 	private UnscheduledTask unscheduledTask_;
 
-	public SchedulingData(Collection<Schedulable> schedulables, TimeLord timeLord,
+	public SchedulingData(Collection<Schedulable> schedulables, Collection<Location> locations, TimeLord timeLord,
 			UnscheduledTask unscheduledTask) {
 		this.allSchedulables_ = schedulables;
+		this.locations_ = locations;
 		this.timeLord_ = timeLord;
 		this.unscheduledTask_ = unscheduledTask;
 	}
@@ -25,6 +28,10 @@ public class SchedulingData
 	 */
 	public Collection<Schedulable> getAllResources() {
 		return this.allSchedulables_;
+	}
+	
+	public Collection<Location> getLocations(){
+		return this.locations_;
 	}
 
 	/**
