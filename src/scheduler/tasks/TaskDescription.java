eb3b4adaa66996311f7cb0f0731eb3d9d8b7c3b9
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-04-03 16:47:09 +0000

    TaskDescription.

--- src/scheduler/tasks/TaskDescription.java
+++ src/scheduler/tasks/TaskDescription.java
@@ -8,13 +8,31 @@ import be.kuleuven.cs.som.annotate.Basic;
 import controllers.interfaces.DescriptionIN;
 import exceptions.InvalidHospitalDateException;
 
+/**
+ * This class is an abstract representation of a TaskDescription. The
+ * TaskDescription contains all the information needed to create and schedule a
+ * Task.
+ */
 public abstract class TaskDescription implements DescriptionIN
 {
 	private final long duration_;
 	private final long extraTime_;
 	private final HospitalDate creationDate_;
-	protected Result result;
+	protected Result result_;
 
+	/**
+	 * Default constructor of the TaskDescription.
+	 * 
+	 * @param duration
+	 *            The duration of the task.
+	 * @param extraTime
+	 *            The amount of time this task need to be scheduled later than
+	 *            the current time.
+	 * @param creationDate
+	 *            The date on which this description has been created.
+	 * @throws InvalidHospitalDateException
+	 *             The given creationDate was not valid.
+	 */
 	public TaskDescription(long duration, long extraTime, HospitalDate creationDate)
 			throws InvalidHospitalDateException {
 		if (!isValidAmountOfExtraTime(extraTime))
@@ -25,36 +43,83 @@ public abstract class TaskDescription implements DescriptionIN
 		this.duration_ = duration;
 		this.extraTime_ = extraTime;
 		this.creationDate_ = creationDate;
-
 	}
 
+	/**
+	 * Returns all the requirements that are needed to forfill this task.
+	 * 
+	 * @return All the requirements that are needed to forfill this task.
+	 */
 	public abstract Collection<Requirement> getAllRequirements();
 
-	@Basic
-	public final HospitalDate getCreationTime() {
-		return new HospitalDate(this.creationDate_);
-	}
-
+	/**
+	 * Returns the duration of the TaskDescription.
+	 * 
+	 * @return The duration of the TaskDescription.
+	 */
 	@Basic
 	public final long getDuration() {
 		return this.duration_;
 	}
 
+	/**
+	 * Returns the amount of time this task need to be scheduled later than the
+	 * current time of the TaskDescription.
+	 * 
+	 * @return The amount of time this task need to be scheduled later than the
+	 *         current time of the TaskDescription.
+	 */
 	@Basic
 	public final long getExtraTime() {
 		return this.extraTime_;
 	}
 
+	/**
+	 * Returns the creationDate of the TaskDescription.
+	 * 
+	 * @return The creationDate of the TaskDescription.
+	 */
+	@Basic
+	public final HospitalDate getCreationTime() {
+		return new HospitalDate(this.creationDate_);
+	}
+
+	/**
+	 * Initializes the task.
+	 * 
+	 * @param task
+	 *            The task that has to be initialized.
+	 */
 	public abstract <T extends TaskDescription> void initTask(Task<T> task);
 
+	/**
+	 * Deinitializes the task. Undoes the things that have been done in
+	 * 'initTask'.
+	 * 
+	 * @param task
+	 *            The task that has to be deinitialized.
+	 */
 	public abstract <T extends TaskDescription> void deInit(Task<T> task);
 
+	/**
+	 * Checks whether the given extraTime is valid.
+	 * 
+	 * @param extraTime
+	 *            The extraTime that has to be checked.
+	 * @return True if the given extraTime is not negative.
+	 */
 	private boolean isValidAmountOfExtraTime(long extraTime) {
 		return extraTime >= 0;
 	}
 
-	private boolean isValidSystemTime(HospitalDate currentSystemTime) {
-		return currentSystemTime != null;
+	/**
+	 * Checks whether the given systemTime is valid.
+	 * 
+	 * @param systemTime
+	 *            The systemTime that has to be checked.
+	 * @return True if the given systemTime is not null.
+	 */
+	private boolean isValidSystemTime(HospitalDate systemTime) {
+		return systemTime != null;
 	}
-
 }
