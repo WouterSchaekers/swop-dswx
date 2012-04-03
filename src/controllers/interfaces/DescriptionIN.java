375381ae685fa2888de3f2e57aff5a24ae6de161
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-04-03 17:13:00 +0000

    NU IS ER WEL EEN RETURN!

--- src/controllers/interfaces/DescriptionIN.java
+++ src/controllers/interfaces/DescriptionIN.java
@@ -2,13 +2,37 @@ package controllers.interfaces;
 
 import result.Result;
 
+/**
+ * This is the public interface for the Description class.
+ */
 public interface DescriptionIN
 {
+	/**
+	 * Returns whether the Description needs a result.
+	 * 
+	 * @return True if the Description needs a result.
+	 */
 	public boolean needsResult();
 
+	/**
+	 * Sets the result of the Description.
+	 * 
+	 * @param result
+	 *            The result of the Description.
+	 */
 	public void setResult(String result);
 
+	/**
+	 * Returns the Result of the Description.
+	 * 
+	 * @return The Result in the Description.
+	 */
 	public Result getResult();
-	
+
+	/**
+	 * Returns the safe interface of the PatientFile.
+	 * 
+	 * @return The safe interface of the PatientFile.
+	 */
 	public PatientFileIN getPatientFile();
 }
