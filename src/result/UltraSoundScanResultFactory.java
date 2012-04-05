7008ec939c0018a79df103cd6cc5f21b3abae6dc
dietergeboers <dietergeboers@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-04-05 20:11:30 +0000

    Ultrasound & xray result fixed

--- src/result/UltraSoundScanResultFactory.java
+++ src/result/UltraSoundScanResultFactory.java
@@ -1,11 +1,30 @@
 package result;
 
+import exceptions.FactoryInstantiationException;
+import result.UltrasoundScanResult.ScanedMassInfo;
+
 public class UltraSoundScanResultFactory implements ResultFactory
 {
 
+	private String scanInfo_;
+	private ScanedMassInfo scanedMassNature_;
+
 	@Override
-	public Result create() {
-		return null;
+	public Result create() throws FactoryInstantiationException {
+		if(! isready())
+			throw new FactoryInstantiationException("ultrasoundscan result factory not sufficiently instantiated.");
+		return new UltrasoundScanResult(scanInfo_,scanedMassNature_);
 	}
 
+	private boolean isready() {
+		return scanInfo_!=null&&scanedMassNature_!=null;
+	}
+	public void setScannInfo(String scanInfo)
+	{
+		this.scanInfo_=scanInfo;
+	}
+	public void setScannedMassNature(ScanedMassInfo info)
+	{
+		this.scanedMassNature_=info;
+	}
 }
