68bf99c44d710a98db7a0903bd854332c20c5ca6
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-04-05 11:25:42 +0000

    help package done.

--- src/help/Collections.java
+++ src/help/Collections.java
@@ -3,15 +3,25 @@ package help;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * Help class presenting methods that can filter.
+ */
 public final class Collections
 {
-	public static final <T> Collection<T> filter(Collection<T> tofilter,
-			Filter f) {
-		ArrayList<T> a = new ArrayList<T>();
-		for (T t : tofilter)
-			if (f.allows(t))
-				a.add(t);
-
-		return a;
+	/**
+	 * Filters a Collection with a certain Filter.
+	 * 
+	 * @param toFilter
+	 *            The Collection that has to be filtered.
+	 * @param filter
+	 *            The Filter that will filter the Collection.
+	 * @return A Collection of toFilter items that are allowed by the Filter.
+	 */
+	public static final <T> Collection<T> filter(Collection<T> toFilter, Filter filter) {
+		ArrayList<T> rv = new ArrayList<T>();
+		for (T element : toFilter)
+			if (filter.allows(element))
+				rv.add(element);
+		return rv;
 	}
 }
--- src/help/Filter.java
+++ src/help/Filter.java
@@ -1,6 +1,16 @@
 package help;
 
+/**
+ * Interface representing a Filter
+ */
 public interface Filter
 {
+	/**
+	 * Checks whether the given object is allowed by the Filter.
+	 * 
+	 * @param arg
+	 *            The object that has to be checked.
+	 * @return True if the given object is allowed by the Filter.
+	 */
 	public <T> boolean allows(T arg);
 }
