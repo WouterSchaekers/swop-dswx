62d9becf10bf9c57f0e5e538e64a3b8895cb92e6
vegetas.fate@gmail.com <vegetas.fate@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-04-06 11:17:30 +0000

    bugfix & testscenario

--- src/controllers/PrescribeTreatmentController.java
+++ src/controllers/PrescribeTreatmentController.java
@@ -82,6 +82,7 @@ public class PrescribeTreatmentController extends NeedsLoginAndPatientFileContro
 			throw new InvalidDiagnoseException("The selected diagnose is invalid!");
 		if (!getAllPossibleDiagnosis().contains(selected))
 			throw new InvalidDiagnoseException("Trying to add a treatment to a diagnose that this doctor has not made!");
+		treatmentFactory.setCreationDate(hospital.getTimeKeeper().getSystemTime());
 		@SuppressWarnings("deprecation")
 		Task<?> createdTreatment = hospital.getTaskManager().add(treatmentFactory.create());
 
