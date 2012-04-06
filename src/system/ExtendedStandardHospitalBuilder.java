e11d7039144dce760dcd6ca0d20fe47694336480
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-04-06 16:59:48 +0000

    system gedoct tot Hospital.

--- src/system/ExtendedStandardHospitalBuilder.java
+++ src/system/ExtendedStandardHospitalBuilder.java
@@ -21,7 +21,7 @@ public class ExtendedStandardHospitalBuilder
 	}
 
 	/**
-	 * @return A Hospital
+	 * @return An Extended Standard Hospital.
 	 */
 	@system.SystemAPI
 	public Hospital build() {
@@ -37,17 +37,30 @@ public class ExtendedStandardHospitalBuilder
 		return rv;
 	}
 
+	/**
+	 * Adds TreatmentFactories to the given Hospital.
+	 * 
+	 * @param rv
+	 *            The given Hospital.
+	 */
 	private void addMedicalTests(Hospital rv) {
 		rv.addTreatmentFactory(new CastFactory());
 		rv.addTreatmentFactory(new MedicationFactory());
 		rv.addTreatmentFactory(new SurgeryFactory());
-
 	}
 
-	private void addDoctor(Campus campus, String string) {
+	/**
+	 * Adds a Doctor to the given Campus.
+	 * 
+	 * @param campus
+	 *            The Campus where the Doctor has to be added to.
+	 * @param doctor
+	 *            The name of the Doctor.
+	 */
+	private void addDoctor(Campus campus, String doctor) {
 		DoctorFactory factory = new DoctorFactory();
 		factory.setLocation(campus);
-		factory.setName(string);
+		factory.setName(doctor);
 		try {
 			campus.getHospital().getUserManager().createUser(factory);
 		} catch (Exception e) {
@@ -56,6 +69,12 @@ public class ExtendedStandardHospitalBuilder
 
 	}
 
+	/**
+	 * Adds Machines to the given Campus.
+	 * 
+	 * @param campus
+	 *            The Campus where Machines need to be added to.
+	 */
 	private void addAllMachines(Campus campus) {
 		int i = 0;
 		for (MachineBuilder builder : campus.getMachinePool().getAllBuilders()) {
@@ -71,25 +90,38 @@ public class ExtendedStandardHospitalBuilder
 
 	}
 
-	private void addPatient(Campus campus, String string) {
+	/**
+	 * Adds a Patient to the given Campus.
+	 * 
+	 * @param campus
+	 *            The Campus where the Patient has to be added to.
+	 * @param patient
+	 *            The name of the Patient.
+	 */
+	private void addPatient(Campus campus, String patient) {
 		try {
-			campus.getHospital().getPatientFileManager().registerPatient(string, campus);
+			campus.getHospital().getPatientFileManager().registerPatient(patient, campus);
 		} catch (Exception e) {
 			;
 		}
-
 	}
 
-	private void addNurse(Campus campus, String string) {
+	/**
+	 * Adds a Nurse to the given Campus.
+	 * 
+	 * @param campus
+	 *            The Campus where the Nurse has to be added to.
+	 * @param nurse
+	 *            The name of the Nurse.
+	 */
+	private void addNurse(Campus campus, String nurse) {
 		NurseFactory factory = new NurseFactory();
 		factory.setLocation(campus);
-		factory.setName(string);
+		factory.setName(nurse);
 		try {
 			campus.getHospital().getUserManager().createUser(factory);
 		} catch (Exception e) {
 			;
 		}
-
 	}
-
 }
