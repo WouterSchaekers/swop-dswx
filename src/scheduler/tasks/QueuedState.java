07201a7399a8a58bd390e659ee8a5a5a19c26eca
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-04-06 15:07:39 +0000

    Task + TaskStates gedoct.

--- src/scheduler/tasks/QueuedState.java
+++ src/scheduler/tasks/QueuedState.java
@@ -4,51 +4,88 @@ import java.util.Collection;
 import scheduler.Schedulable;
 import system.Location;
 
+/**
+ * Class representing the queued state of the Task.
+ */
 class QueuedState implements TaskState
 {
 	private TaskData data_;
 
+	/**
+	 * Default constructor.
+	 */
 	public QueuedState(TaskData taskData) {
 		if (!isValidNewData(taskData))
 			throw new IllegalArgumentException("Invalid data given to QueuedState!");
 		this.data_ = taskData;
 	}
 
+	/**
+	 * @return The data for this Task.
+	 */
 	@Override
 	public TaskData getData() {
 		return this.data_;
 	}
 
+	/**
+	 * Cycles through to the next state: Queued -> Scheduled
+	 * 
+	 * @param newData
+	 *            A data object that contains the data for the new state.
+	 */
 	@Override
 	public TaskState nextState(TaskData data) {
 		return new ScheduledState(data);
 	}
 
+	/**
+	 * Returns the Location of this Task.
+	 */
 	@Override
 	public Location getLocation() {
 		throw new IllegalStateException("Queued states do not have locations!");
 	}
 
+	/**
+	 * Returns the resources used for this Task.
+	 */
 	@Override
 	public Collection<Schedulable> getResources() {
 		return data_.getAllAvailableResources();
 	}
 
+	/**
+	 * Returns false;
+	 */
 	@Override
 	public boolean isFinished() {
 		return false;
 	}
 
+	/**
+	 * Returns true.
+	 */
 	@Override
 	public boolean isQueued() {
 		return true;
 	}
 
+	/**
+	 * Returns false.
+	 */
 	@Override
 	public boolean isScheduled() {
 		return false;
 	}
 
+	/**
+	 * Checks whether the given TaskData is valid.
+	 * 
+	 * @param data
+	 *            The TaskData that has to be checked.
+	 * @return True if the TaskData is valid.
+	 */
 	@Override
 	public boolean isValidNewData(TaskData data) {
 		return data != null && data.getDescription() != null;
--- src/scheduler/tasks/ScheduledState.java
+++ src/scheduler/tasks/ScheduledState.java
@@ -4,51 +4,88 @@ import java.util.Collection;
 import scheduler.Schedulable;
 import system.Location;
 
+/**
+ * Class representing the scheduled state of the Task.
+ */
 class ScheduledState implements TaskState
 {
 	private TaskData data_;
 
+	/**
+	 * Default constructor.
+	 */
 	public ScheduledState(TaskData taskData) {
 		if (!isValidNewData(taskData))
 			throw new IllegalArgumentException("Invalid data given to ScheduledState!");
 		this.data_ = taskData;
 	}
 
+	/**
+	 * @return The data for this Task.
+	 */
 	@Override
 	public TaskData getData() {
 		return data_;
 	}
 
+	/**
+	 * Cycles through to the next state: Scheduled -> Finished
+	 * 
+	 * @param newData
+	 *            A data object that contains the data for the new state.
+	 */
 	@Override
 	public TaskState nextState(TaskData data) {
 		return new FinishedState(data);
 	}
 
+	/**
+	 * Returns the Location of this Task.
+	 */
 	@Override
 	public Location getLocation() {
 		return data_.getLocation();
 	}
 
+	/**
+	 * Returns the resources used for this Task.
+	 */
 	@Override
 	public Collection<Schedulable> getResources() {
 		return data_.getResources();
 	}
 
+	/**
+	 * Returns false.
+	 */
 	@Override
 	public boolean isFinished() {
 		return false;
 	}
 
+	/**
+	 * Returns false.
+	 */
 	@Override
 	public boolean isQueued() {
 		return false;
 	}
 
+	/**
+	 * Returns true.
+	 */
 	@Override
 	public boolean isScheduled() {
 		return true;
 	}
 
+	/**
+	 * Checks whether the given TaskData is valid.
+	 * 
+	 * @param data
+	 *            The TaskData that has to be checked.
+	 * @return True if the TaskData is valid.
+	 */
 	@Override
 	public boolean isValidNewData(TaskData data) {
 		return data != null && data.getDescription() != null;
