b86a28e6d2d681e886f9c00e5e25018fa273e976
w29.ftw@gmail.com <w29.ftw@gmail.com@b180c425-cf1a-e361-07c7-cac54ae909d2>
2012-04-06 16:46:04 +0000

    System deels gedoct.

--- src/system/ExtendedStandardHospitalBuilder.java
+++ src/system/ExtendedStandardHospitalBuilder.java
@@ -11,29 +11,27 @@ import users.NurseFactory;
 public class ExtendedStandardHospitalBuilder
 {
 	/**
-	 * Creates a hospital with :
-	 * With "Campus 1" and "Campus 2"
-	 * and a hospital admin with the name: "Thibault Leemans"
-	 * nurse jenny </br>
-	 * nurse jasmine</br>
-	 * doctor jonathan</br>
-	 * doctor stefaan</br>
-	 * patient daan</br>
+	 * Creates a hospital with : With "Campus 1" and "Campus 2" and a hospital
+	 * admin with the name: "Thibault Leemans" nurse jenny </br> nurse
+	 * jasmine</br> doctor jonathan</br> doctor stefaan</br> patient daan</br>
 	 */
 	@system.SystemAPI
-	public ExtendedStandardHospitalBuilder()
-	{
+	public ExtendedStandardHospitalBuilder() {
 		;
 	}
+
+	/**
+	 * @return A Hospital
+	 */
 	@system.SystemAPI
 	public Hospital build() {
 		Hospital rv = new StandardHospitalBuilder().build();
-		Campus campus =rv.getAllCampuses().iterator().next();
-		addNurse(campus,"jenny");
-		addNurse(campus,"Jasmine");
-		addDoctor(campus,"jonathan");
-		addDoctor(campus,"Stefaan");
-		addPatient(campus,"daan");
+		Campus campus = rv.getAllCampuses().iterator().next();
+		addNurse(campus, "jenny");
+		addNurse(campus, "Jasmine");
+		addDoctor(campus, "jonathan");
+		addDoctor(campus, "Stefaan");
+		addPatient(campus, "daan");
 		addAllMachines(campus);
 		addMedicalTests(rv);
 		return rv;
@@ -43,40 +41,43 @@ public class ExtendedStandardHospitalBuilder
 		rv.addTreatmentFactory(new CastFactory());
 		rv.addTreatmentFactory(new MedicationFactory());
 		rv.addTreatmentFactory(new SurgeryFactory());
-		
+
 	}
+
 	private void addDoctor(Campus campus, String string) {
 		DoctorFactory factory = new DoctorFactory();
 		factory.setLocation(campus);
 		factory.setName(string);
 		try {
 			campus.getHospital().getUserManager().createUser(factory);
-		} catch (Exception e) {;
+		} catch (Exception e) {
+			;
 		}
-		
+
 	}
 
 	private void addAllMachines(Campus campus) {
 		int i = 0;
-		for(MachineBuilder builder : campus.getMachinePool().getAllBuilders())
-		{
+		for (MachineBuilder builder : campus.getMachinePool().getAllBuilders()) {
 			builder.setLocation(campus);
-			builder.setLocationWithinCampus("Room:"+i);
+			builder.setLocationWithinCampus("Room:" + i);
 			builder.setSerial(i++);
 			try {
 				campus.getMachinePool().addMachine(builder);
-			} catch (Exception e) {;
+			} catch (Exception e) {
+				;
 			}
 		}
-		
+
 	}
 
 	private void addPatient(Campus campus, String string) {
 		try {
 			campus.getHospital().getPatientFileManager().registerPatient(string, campus);
-		} catch (Exception e) {;
+		} catch (Exception e) {
+			;
 		}
-		
+
 	}
 
 	private void addNurse(Campus campus, String string) {
@@ -85,10 +86,10 @@ public class ExtendedStandardHospitalBuilder
 		factory.setName(string);
 		try {
 			campus.getHospital().getUserManager().createUser(factory);
-		} catch (Exception e) {;
+		} catch (Exception e) {
+			;
 		}
-		
+
 	}
 
-	
 }
--- src/system/Location.java
+++ src/system/Location.java
@@ -3,7 +3,7 @@ package system;
 import controllers.interfaces.LocationIN;
 
 /**
- * This class represents the current location of a certain person.
+ * This class represents the current location of a certain Object.
  */
 public interface Location extends LocationIN
 {
